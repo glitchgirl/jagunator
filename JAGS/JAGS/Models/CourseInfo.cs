ae7a990c77e4af63ef9033a56936cebaac992acb
keasheo <31492167+keasheo@users.noreply.github.com>
2018-02-24 14:07:30 -0500

    EditCourse
    Added some edit course stuff

---
+++ JAGS/JAGS/Models/CourseInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace JAGS.Models
+{
+    public class CourseInfo
+    {
+        [Required]
+        [Display(Name = "Instructor Name")]
+        public string IntructorName { get; set; }
+
+        [Required]
+        [Display(Name ="Course Name")]
+        public string CourseName { get; set; }
+
+        [Required]
+        [Display(Name ="Course ID")]
+        public string CourseID { get; set; }
+
+        public List<CampusLocation> CampusNames { get; set; }
+        public List<ClassroomSize> ClassroomStudentSize { get; set; }
+
+
+        public CourseInfo()
+        {
+            ClassroomStudentSize = new List<ClassroomSize>();
+            CampusNames = new List<CampusLocation>();
+        }
+
+    }
+}
