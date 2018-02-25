41ed507217b946e2e37d6d57ed875532e24dae31
Technaholic <technaholic@gmail.com>
2018-02-25 16:27:33 -0500

    update layout
    update layout

--- JAGS/JAGS/Controllers/HomeController.cs
+++ JAGS/JAGS/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using JAGS.Models;
-using System.Web.Mvc;
+//using System.Web.Mvc;
 
 namespace JAGS.Controllers
 {
