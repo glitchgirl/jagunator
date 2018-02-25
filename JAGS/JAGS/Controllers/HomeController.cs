39537c65e8ced3cd6a68ed28d5deacbcf52daa7b
Technaholic <technaholic@gmail.com>
2018-02-25 17:49:53 -0500

    csv working
    csv test working

--- JAGS/JAGS/Controllers/HomeController.cs
+++ JAGS/JAGS/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using JAGS.Models;
 //using System.Web.Mvc;
+using System.Web;
 
 namespace JAGS.Controllers
 {
@@ -66,8 +67,14 @@ namespace JAGS.Controllers
         [HttpPost]
         public ActionResult Index(LoginModel model)
         {
+            var filepath = "/Users/administrator/Documents/GitHub/jagunator/JAGS/JAGS/Users/" + model.Login + ".csv";
+            var absfilepath = System.IO.File.Exists(filepath);
+            if (absfilepath)
+            {
+                return View("CreateEditUser", model);
+            }
             Console.Write("We have pressed the login button!");
-            if (ModelState.IsValid)
+            if (model.Login == "Admin")
             {
                 return View("CreateEdit", model);
             }
