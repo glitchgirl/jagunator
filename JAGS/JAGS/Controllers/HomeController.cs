6b37d847f0b070efb05582649342b33924f286da
Technaholic <technaholic@gmail.com>
2018-03-04 10:51:30 -0500

    fix js
    fix js

--- JAGS/JAGS/Controllers/HomeController.cs
+++ JAGS/JAGS/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using JAGS.Models;
+//using System.Web
 //using static System.Web.Mvc.SelectListItem;
 //using System.Web.Hosting;
 //using Environment;
