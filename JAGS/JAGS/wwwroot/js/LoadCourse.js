c34e349c10d2b146016025f0103c4bffc5cb4131
Jareth Childs <keasheo@gmail.com>
2018-03-04 10:06:20 -0500

    Trying some js

---
+++ JAGS/JAGS/wwwroot/js/LoadCourse.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    $('.CourseList').on("change", function () {
+        var selected = $(this).val() + '.csv';
+        $(document).ready(function () {
+            $.ajax({
+                type: "GET",
+                url: selected,
+                dataType: "text",
+                success: function (data) { LoadData(data); }
+            });
+        });
+
+        function LoadData(CourseInfo) {
+            var AllText = CourseInfo.split(/\r\n|\n/);
+            var headers = AllText[0].split(',');
+            var info = [];
+            var data;
+
+            for (var i = 1; i < AllText.length; i++) {
+                data = AllText[i].split(',');
+            }
+
+            $('#InstructorName').val(data[0]);
+            $('#CourseName').val(data[1]);
+            $('#CourseID').val(data[2]);
+            $('#CampusNames').val(data[3]);
+            $('#ClassroomStudentSize').val(data[4]);
+        }
+
+
+
+    })
+
+});
