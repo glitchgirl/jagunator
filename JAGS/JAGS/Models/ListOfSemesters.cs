3522c1ec15d13dece7382ea9f00826a0e970d42e
Jareth Childs <keasheo@gmail.com>
2018-03-11 18:48:22 -0400

    OMG TOO MUCH

---
+++ JAGS/JAGS/Models/ListOfSemesters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JAGS.Models
+{
+    public class ListOfSemesters
+    {
+        public int SemesterID { get; set; }
+        public string SemesterNameFromDirectory { get; set; }
+    }
+}
