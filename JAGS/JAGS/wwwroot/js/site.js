04f3eae54628c506b120ed01707390627db3d841
Morgan smith <londonspleen@gmail.com>
2018-03-21 13:21:06 -0400

    js push again

--- JAGS/JAGS/wwwroot/js/site.js
+++ JAGS/JAGS/wwwroot/js/site.js
@@ -74,4 +74,69 @@
         }
 
 
+<<<<<<< HEAD
     });
+=======
+                });
+
+                $('#calendar1').fullCalendar({
+                    header: false,
+                    //columnFormat: 'dddd',
+                    editable: true,
+                    droppable: true, 
+                    dragRevertDuration: 0,
+                    eventLimit: true, 
+                    drop: function(date, jsEvent, ui) { console.log('calendar 1 drop'); console.log(date); console.log(jsEvent); console.log(ui); console.log(this);
+                        /* is the "remove after drop" checkbox checked?*/
+                        if ($('#drop-remove').is(':checked')) {
+                            /* if so, remove the element from the "Draggable Events" list*/
+                            $(this).remove();
+                        }
+
+                        /* if event dropped from another calendar, remove from that calendar*/
+                        if (typeof calEventStatus['calendar'] != 'undefined') {
+                            $(calEventStatus['calendar']).fullCalendar('removeEvents', calEventStatus['event_id']);
+                            /*$(calEventStatus['calendar']).fullCalendar('unselect');*/
+                        }
+
+                        makeEventsDraggable();
+                    },
+                    eventReceive: function( event ) {  console.log('calendar 1 eventReceive');
+                        makeEventsDraggable();
+                    },
+                    eventDrop: function( event, delta, revertFunc, jsEvent, ui, view ) {  console.log('calendar 1 eventDrop');
+                        makeEventsDraggable();
+                    },
+                    eventDragStart: function( event, jsEvent, ui, view ) {
+                        console.log(event); console.log(jsEvent); console.log(ui); console.log(view);
+
+                        /* Add dragging event in global var*/ 
+                        calEventStatus['calendar'] = '#calendar1';
+                        calEventStatus['event_id'] = event._id;
+                        console.log('calendar 1 eventDragStart');
+                    },
+                    eventDragStop: function( event, jsEvent, ui, view ) { console.log('calendar 1 eventDragStop');
+                        
+                        if(isEventOverDiv(jsEvent.clientX, jsEvent.clientY)) {
+                            $('#calendar1').fullCalendar('removeEvents', event._id);
+                            var el = $( "<div class='fc-event'>" ).appendTo( '#external-events-listing' ).text( event.title );
+                            el.draggable({
+                              zIndex: 999,
+                              revert: true, 
+                              revertDuration: 0 
+                            });
+                            el.data('event', { title: event.title, id :event.id, stick: true });
+                        }
+
+                        calEventStatus = []; 
+                        makeEventsDraggable();
+                    },
+                    eventResize: function( event, delta, revertFunc, jsEvent, ui, view ) {
+                        makeEventsDraggable();
+                    },
+                    viewRender: function() { console.log('calendar 1 viewRender');
+                        makeEventsDraggable();
+                    },
+                });
+            });
+>>>>>>> 87215a67b0ffe2acafb726e57d58a8cc1d3bc26f
