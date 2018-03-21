bb00f0bafc5f5dd62bb938d6313881445a5921a4
Morgan smith <londonspleen@gmail.com>
2018-03-21 13:18:25 -0400

    logo possible solution to class things

--- JAGS/JAGS/Controllers/HomeController.cs
+++ JAGS/JAGS/Controllers/HomeController.cs
@@ -481,6 +481,6 @@ namespace JAGS.Controllers
             ViewBag.sessiontype = HttpContext.Session.GetString(SessionUserType);
             ViewBag.loginname = HttpContext.Session.GetString(SessionUserName);
             return View(model);
-        }
-    }//namespace
-}
+        }//createeditschedule
+    }//class
+}//namespace
