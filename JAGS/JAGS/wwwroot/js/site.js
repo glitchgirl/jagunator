23544b2712a32100856f3bde9013ca236e8e6ee9
Technaholic <technaholic@gmail.com>
2018-03-26 12:28:47 -0400

    Fix logo in menu bar and header from calendar
    fix logo in menu bar and header in calendar.

--- JAGS/JAGS/wwwroot/js/site.js
+++ JAGS/JAGS/wwwroot/js/site.js
@@ -26,11 +26,7 @@
         -----------------------------------------------------------------*/
 
         $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
+            header: false,
             editable: true,
             droppable: true, // this allows things to be dropped onto the calendar
             dragRevertDuration: 0,
